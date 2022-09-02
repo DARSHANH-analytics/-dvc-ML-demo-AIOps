5f6e0f7a18d816aa2f7512e5e48a5d728735d7ff
Darshan H <darshan8983@gmail.com>
2022-09-02 15:17:02 +0530

    stage 01 created

--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -1,3 +1,4 @@
+from genericpath import exists
 import yaml
 import os
 
@@ -5,3 +6,8 @@ def read_yaml(path_of_yaml_file:str) -> dict:
     with open(path_of_yaml_file) as yaml_file:
         content = yaml.safe_load(yaml_file)
     return content
+
+def create_directory(dirs):
+    for dir_path in dirs:
+        os.makedirs(dir_path,exist_ok=True)
+        print(f"directory is created at {dir_path}")
