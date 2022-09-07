1a05fcadfe7eb56c675c1f71a57421d75f729922
Darshan H <darshan8983@gmail.com>
2022-09-07 11:17:04 +0530

    evaluate stage added

--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -1,6 +1,7 @@
 from genericpath import exists
 import yaml
 import os
+import json
 
 def read_yaml(path_of_yaml_file:str) -> dict:
     with open(path_of_yaml_file) as yaml_file:
@@ -14,4 +15,9 @@ def create_directory(dirs):
 
 def save_local_df(data, data_path, index_status=False):
     data.to_csv(data_path, index=index_status)
-    print(f"data is saved at {data_path}")        
+    print(f"data is saved at {data_path}")
+
+def save_reports(report: dict, report_path: str, indentation=4):
+    with open(report_path, "w") as f:
+        json.dump(report, f, indent=indentation)
+    print(f"reports are saved at {report_path}")
